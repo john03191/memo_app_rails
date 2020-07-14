6a4bbab8664b82f062b5d54704d611188694d038
john03191 <john03tgck01a@gmail.com>
2020-07-14 09:57:37 +0900

    created_memo_app

---
+++ app/controllers/memo_controller.rb
@@ -0,0 +1,53 @@
+class MemoController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.order(created_at: "DESC")
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:notice] = "投稿できました"
+      redirect_to root_path
+    else
+      flash[:alert] = "文字を入力してください"
+      redirect_to new_memo_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "投稿できました"
+      redirect_to root_path
+    else
+      flash[:alert] = "文字を入力してください"
+      redirect_to edit_memo_path(@post.id)
+    end
+  end
+  
+  def destroy
+    if @post.destroy
+      flash[:delete] = "削除しました"
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
